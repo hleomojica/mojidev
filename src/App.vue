63ccd5e27ecaabf5a7562ce141727d86236eaf27
Henrry Mojica <hleomojica@Henrrys-MacBook-Pro.local>
2021-11-15 12:21:14 +1100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "App",
+  // created() {
+
+  //   const currentPath = this.$router.history.current.path;
+
+  //   if (window.localStorage.getItem("authenticated") === "false") {
+  //     this.$router.push("/login");
+  //   }
+  //   window.localStorage.setItem("authenticated")
+
+  //   if (currentPath === "/" || currentPath === "/app") {
+  //     this.$router.push("/app/dashboard");
+  //   }
+  // },
+};
+</script>
+
+<style src="./styles/theme.scss" lang="scss" />
